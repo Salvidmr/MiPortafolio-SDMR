d8a8340bafa2cb1b765ac85ed0712394cf075d46
Salvidmr <165647969+Salvidmr@users.noreply.github.com>
2025-08-15 17:00:43 +0200

    Curso Udemy JavaScript de Fernando Herrera

--- CURSO JAVASCRIPT/06-advanced/src/concepts/03-promises.js
+++ CURSO JAVASCRIPT/06-advanced/src/concepts/03-promises.js
@@ -10,6 +10,14 @@ export const promiseComponent = ( element ) => {
         element.innerHTML = hero.name;
     }
 
+    const renderTwoHeros = (hero1, hero2) => {
+        element.innerHTML = `
+            <h3>${hero1.name}</h3
+            <h3>${hero2.name}</h3
+
+        `;
+    }
+
     const renderError = ( error ) => {
         element.innerHTML = `
         <h1>Error</h1>
@@ -19,9 +27,39 @@ export const promiseComponent = ( element ) => {
     const id1 = '5d86371f233c9f2425f16916';
     const id2 = '5d86371fd55e2e2a30fe1ccb';
 
-    findHero(id1)
-    .then(renderHero)
+    let hero1, hero2;
+
+    Promise.all([
+        findHero(id1),
+        findHero(id2),
+    ])
+    .then(([hero1, hero2]) => {
+        renderTwoHeros(hero1, hero2);
+        })
     .catch(renderError);
+
+
+    // findHero(id1)
+    // .then( ( hero ) => {
+    //     hero1 = hero;
+    //     return findHero(id2);
+    // })
+    // .then ( ( hero2 ) => {
+    //     renderTwoHeros(hero1, hero2);
+    // })
+    // .catch(renderError)
+
+
+    // findHero(id1)
+    // .then((hero1) => {
+    //     findHero(id2)
+    //     .then(hero2 => {
+    //         renderTwoHeros(hero1, hero2);
+    //     })
+    //     .catch(renderError);
+
+    // })
+    // .catch(renderError);
 
 }
 
